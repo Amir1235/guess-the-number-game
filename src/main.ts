af3b225355fa94df57636cca85266f79762feabb
amircoll1235 <amir_abukar@outlook.com>
2024-12-24 17:51:46 +0100

    compare withrandom number if too high or lwo

--- src/main.ts
+++ src/main.ts
@@ -1,19 +1,25 @@
 const input = document.getElementById("write") as HTMLInputElement;
 const button = document.getElementById("sendButton") as HTMLButtonElement;
 const feedback = document.getElementById("feedback");
+const rand = Math.floor(Math.random() * 100) + 1;  
+const div1 = document.createElement("div");
+const textNode = document.createTextNode(rand.toString());
+div1.appendChild(textNode);
+document.body.appendChild(div1);
 
-
-button?.addEventListener("click",()=>{
+button?.addEventListener("click", () => {
   const inputvalue = input.value;
   const numValue = Number(inputvalue);
-  
-  if(numValue >=1 && numValue<=100){
-    feedback!.innerHTML = input.value;
 
-  } else{
-    feedback!.innerHTML ="Please enter a number between 1 and 100"
+  if (numValue >= 1 && numValue <= 100) {
+    if (rand > numValue) {
+      feedback!.innerHTML = "Too low";
+    } else if (rand < numValue) {
+      feedback!.innerHTML = "Too high";
+    } else {
+      feedback!.innerHTML = "Correct!";
+    }
+  } else {
+    feedback!.innerHTML = "Please enter a number between 1 and 100.";
   }
-
-})
-
-
+});
