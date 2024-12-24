26f3cd6b41f850a89df7d052d567f4bafe6f5171
amircoll1235 <amir_abukar@outlook.com>
2024-12-24 17:33:59 +0100

    start of the game

--- src/main.ts
+++ src/main.ts
@@ -1,24 +1,19 @@
-import './style.css'
-import typescriptLogo from './typescript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.ts'
+const input = document.getElementById("write") as HTMLInputElement;
+const button = document.getElementById("sendButton") as HTMLButtonElement;
+const feedback = document.getElementById("feedback");
 
-document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
-  <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://www.typescriptlang.org/" target="_blank">
-      <img src="${typescriptLogo}" class="logo vanilla" alt="TypeScript logo" />
-    </a>
-    <h1>Vite + TypeScript</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite and TypeScript logos to learn more
-    </p>
-  </div>
-`
 
-setupCounter(document.querySelector<HTMLButtonElement>('#counter')!)
+button?.addEventListener("click",()=>{
+  const inputvalue = input.value;
+  const numValue = Number(inputvalue);
+  
+  if(numValue >=1 && numValue<=100){
+    feedback!.innerHTML = input.value;
+
+  } else{
+    feedback!.innerHTML ="Please enter a number between 1 and 100"
+  }
+
+})
+
+
